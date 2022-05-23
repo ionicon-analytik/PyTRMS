d4de79705f5fb1440292612f375d33e8fd3664e1
Moritz Koenemann <moritz.k-man@arcor.de>
2022-05-23 14:19:20 +0200

    Use the 'Measurement' like a state machine.

--- pytrms/measurement.py
+++ pytrms/measurement.py
@@ -3,85 +3,143 @@ import time
 
 #from .abstract.traceable import Traceable
 
+# TODO :: States: NICHT GESTARTET --> RUNNING --> STOPPED
+#  das soll sich verbrauchen" und dann nicht mehr gestartet werden koennen
+#  anschliessend kann man's postprocessen
+#  es gilt: ein Measurement, ein filename!
+
 
 class Measurement:
     '''
     Base class for PTRMS measurements or batch processing.
 
+    A Measurement has one unique filename. 
+
+    A Measurement can be preparing, running or finished.
+
+    A prepared Measurement can be started.
+    A running Measurement can be stopped.
+    A finished Measurement cannot be started again.
+
     Subclasses of `Process` must implement the `__iter__()` function to guarantee
     the common behaviour for online and offline processing with one or multiple
     datafiles.
+
+    mm = Measurement()
     '''
     time_format = "%Y-%m-%d_%H-%M-%S"
+    prefix = ''
 
-    @staticmethod
-    def _name_convention(fmt, prefix):
-        '''Factory for the next filename.'''
-        return prefix + '_' + time.strftime(fmt, time.localtime())
+    def _new_state(self, newstate):
+        self.__class__ = newstate
 
-    def _make_filename(self):
-        basename = self.basename if self.basename else self._name_convention(self.time_format, self.prefix)
-        basename += '.h5'
-
-        return os.path.join(self.home, basename)
-
-    @property
-    def is_running(self):
+    def __init__(self, path, client=None):
         if client is None:
-            return False
-
-        return True  # TODO :: status abfragen!! (Kandidat: )
-        self.client.get('ACQ_SRV_CurrentState')
-
-    def __init__(self, path, client=None, prefix=''):
+            self._new_state(FinishedMeasurement)
+        else:
+            self._new_state(PrepareMeasurement)
         if not len(path):
             path = os.getcwd()
-        self.home = os.path.abspath(os.path.dirname(path))
-        os.makedirs(self.home, exist_ok=True)
-        basename = os.path.basename(path)  # may be empty
-        self.basename, _ = os.path.splitext(basename)
+        home = os.path.dirname(path)
+        os.makedirs(home, exist_ok=True)
+        self.path = os.path.abspath(path)
         self.client = client
-        self.prefix = prefix
-        self.datafiles = []
-        self.settings = {}
-#         self._previous_settings = {}  # TODO : memorize old context
-#   ODER: gleich einfach mal alles vom server holen..??
 
-    def start():
-        if client is None:
-            raise Exception('no connection to instrument')
-
-        if len(self.settings):
-            self.client.set_many(self.settings)
-            print('applied settings')
-        filename = self._make_filename()
-        if os.path.exists(filename):
-            raise Exception(f'{filename} exists and cannot be overwritten')
-
-        self.client.start_measurement(filename)
-        self.datafiles.append(filename)
-        print(f'started measurement at {time.localtime()}')
-
-    def wait(seconds, reason=''):
+    def wait(self, seconds, reason=''):
         if reason:
             print(reason)
         time.sleep(seconds)
 
-    def stop():
-        if client is None:
-            raise Exception('no connection to instrument')
+    @property
+    def is_running(self):
+        raise NotImplementedError()
 
+    def start(self):
+        raise NotImplementedError()
+
+    def stop(self):
+        raise NotImplementedError()
+
+    def set(self, varname, value):
+        raise NotImplementedError()
+
+    def find(self, needle):
+        raise NotImplementedError()
+
+    def iterrows(self):
+        raise NotImplementedError()
+
+
+class PrepareMeasurement(Measurement):
+
+    @property
+    def is_running(self):
+        return False
+
+    def start(self):
+        if os.path.isdir(self.path):
+            basename = time.strftime(Measurement.time_format, time.localtime())
+            basename = Measurement.prefix + basename + '.h5'
+            self.path = os.path.join(self.path, basename)
+
+        if os.path.exists(self.path):
+            raise RuntimeError(f'{self.path} exists and cannot be overwritten')
+
+        self.client.start_measurement(self.path)
+        print(f'started measurement in {self.path}')
+        self._new_state(RunningMeasurement)
+
+    def stop(self):
+        raise RuntimeError('measurement has not been started')
+
+    def set(self, varname, value):
+        self.client.set(varname, value)
+
+    def find(self, needle):
+        raise RuntimeError('measurement has not been started')
+
+    def iterrows(self):
+        raise RuntimeError('measurement has not been started')
+
+
+class RunningMeasurement(Measurement):
+
+    @property
+    def is_running(self):
+        return False
+
+    def start(self):
+        raise RuntimeError('measurement has already started')
+
+    def stop(self):
         self.client.stop_measurement()
         print(f'stopped measurement at {time.localtime()}')
+        self._new_state(FinishedMeasurement)
 
-    def set(varname, value):
+    def set(self, varname, value):
         self.client.set(varname, value)
-        self.settings.update({varname: value})
 
-    def add_file(path):
-        pass
-#         self.sources = sorted(self.sources + [H5Source(path)], key=attrgetter('timezero'))
+    def find(self, needle):
+        self.client.get_traces()
 
+    def iterrows(self):
+        return PollingIterator(self.client)
+
+
+class FinishedMeasurement(Measurement):
+
+    @property
+    def is_running(self):
+        return False
+
+    def start(self):
+        raise RuntimeError('measurement has finished and cannot be restarted')
+
+    def stop(self):
+        raise RuntimeError('measurement has already been stopped')
+
+    def set(self, varname, value):
+        raise RuntimeError('measurement has already been stopped')
 
     def find(self, needle):
         self.client.get_traces()
