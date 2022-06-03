6a88da337e1a11fbbe505109ed33ec453f744b95
Moritz Koenemann <moritz.k-man@arcor.de>
2022-06-03 15:53:29 +0200

    Remove example.

--- examples/measure_breath.py
+++ examples/measure_breath.py
@@ -1,61 +1,55 @@
 #################################################
 #                                               #
-# Script zum definierten Breath Messen          #
+# Script for defined breath measurements        #
 #                                               #
 #################################################
-import os
-import time
-import json
 import winsound
+import pytrms
 
-import ioniclient
 
 def BeepStart():
-    pass
-    #winsound.Beep(440, 500)
-    #winsound.Beep(660, 500)
-    #winsound.Beep(880, 500)
+    winsound.Beep(440, 500)
+    winsound.Beep(660, 500)
+    winsound.Beep(880, 500)
 
 def BeepStop():
-    #winsound.Beep(660, 500)
-    #winsound.Beep(330, 1500)
-    pass
-
-client = ioniclient.IoniClient('127.0.0.1', port=8002)
+    winsound.Beep(660, 500)
+    winsound.Beep(330, 1500)
 
 
-print('# Measurement with filename. Wait to exhale until told.')
-folder = os.path.join('D:', 'Data')
-os.makedirs(folder, exist_ok=True)
-filename = time.strftime("jens_%Y-%m-%d_%H-%M-%S", time.localtime())
-path = os.path.join(folder, filename)
+ptr = pytrms.connect('localhost')
 
-print(client.start_measurement(path))
+patient = input('please enter your name: ')
+filename = patient.replace(' ', '_') + '_%Y-%m-%d_%H-%M-%S.h5'
+path = 'D:/Data/' + filename
+
+print('please wait to exhale until told.')
+
+ptr.start(path)
 time.sleep(10)
+
 BeepStart()
-print('Exhale now.')
+print('Exhale now!')
 time.sleep(110)
-print('Ready to stop')
+
+print('Ready to stop.')
 time.sleep(10)
 BeepStop()
-print(client.stop_measurement())
 
-filename = time.strftime("BK_%Y-%m-%d_%H-%M-%S", time.localtime())
+ptr.stop()
+
+filename = time.strftime(, time.localtime())
 path = os.path.join(folder, filename)
 
-print('Commencing with BG measurment in 1 min')
+print('commencing with BG measurement in 1 minute...')
 time.sleep(60)
-print(client.start_measurement(path))
+
+ptr.start('D:/Data/BK_%Y-%m-%d_%H-%M-%S.h5')
+
 print('BG Started / 120 s to go')
-time.sleep(30)
-print('90s to go')
-time.sleep(30)
-print('60s to go')
-time.sleep(30)
-print('30s to go')
-time.sleep(30)
-print(client.stop_measurement())
-print('BG Stopped')
-print('---------------------------------')
-print('Start over again')
-BeepStop()
+time.sleep(120)
+
+ptr.stop()
+
+print('thank you. please start over again...')
+
