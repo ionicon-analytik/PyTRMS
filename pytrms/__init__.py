afbb5ae99129f89634af9c2750cfb9aea6d4afd5
Moritz Koenemann <moritz.k-man@arcor.de>
2022-06-07 14:04:33 +0200

    Supply simple marker-plot function.

--- pytrms/__init__.py
+++ pytrms/__init__.py
@@ -39,3 +39,28 @@ def connect(host='localhost', port=8002):
 
     return Instrument(_client, _buffer)
 
+
+def plot_marker(signal, marker, **kwargs):
+    '''Plot a `signal` and fill the regions where `marker=True`.
+
+    Returns a tuple of `figure, axis`.
+    '''
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots()
+    if hasattr(signal, 'plot'):
+        subplot = signal.plot(ax=ax)
+        line, *_ = subplot.get_lines()
+    else:
+        line, = ax.plot(signal)
+
+    x_ = line.get_xdata()
+    lo, hi = ax.get_ylim()
+    ax.fill_between(x_, lo, hi, where=marker, color='orange')
+
+    ax.grid(visible=True)
+    if hasattr(signal, 'name'):
+        ax.set_title(signal.name)
+
+    return fig, ax
+
