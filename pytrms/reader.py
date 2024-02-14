dd4cac1f00171483a9387c4b8cf7f3c3ca36d73f
Moritz Koenemann <moritz.koenemann@ionicon.com>
2024-02-14 15:39:45 +0100

    Consolidate files for h5 reader.

--- pytrms/reader.py
+++ pytrms/reader.py
@@ -1,8 +1,15 @@
+"""Module instrument.py
+
+"""
 from abc import abstractmethod
+from collections import namedtuple
 from collections.abc import Iterable
 from functools import partial, lru_cache
+import logging
 
 import h5py
+import pandas as pd
+
 import pandas as pd
 
 # make this file %run'nable from IPython:
@@ -11,6 +18,39 @@ try:
 except ImportError:  # attempted relative import with no known parent package
     from pytrms.helpers import convert_labview_to_posix
 
+
+__all__ = ['H5Reader']
+
+log = logging.getLogger(__name__)
+
+_trace = namedtuple('Trace', ['set', 'act'])
+
+def datainfo2df(h5group, selection=slice(None)):
+    """
+    Split a Data-Info-group into `pd.DataFrame`s for set- and act-values, respectively.
+
+    Note, that the column names are inferred from the Info-dataset and that
+     the columns of act- and set-dataframe need not overlap!
+
+    `h5group`   - a HDF5-group containing datasets "Data" and "Info"
+    `selection` - [slice, optional] load only a part of the TimeCycle-data
+    """
+    names = (info.decode('latin-1') for info in h5group['Info'][0])
+    units = (info.decode('latin-1') for info in h5group['Info'][1])
+
+    df = pd.DataFrame(h5group['Data'][selection], columns=names)
+
+    set_cols = [col for col in df.columns if col.endswith('_Set')]
+    act_cols = [col for col in df.columns if col.endswith('_Act')]
+
+    set_values = df[set_cols]
+    act_values = df[act_cols]
+
+    set_values.columns = [col.replace('_Set', '') for col in set_values.columns]
+    act_values.columns = [col.replace('_Act', '') for col in act_values.columns]
+
+    return _trace(set_values, act_values)
+    
 
 class GroupNotFoundError(KeyError):
     pass
@@ -22,9 +62,29 @@ class H5Reader(Iterable):
     def timezero(self):
         return convert_labview_to_posix(float(self.hf.attrs['FileCreatedTime_UTC']))
 
+    @property
+    def inst_type(self):
+        return str(self.hf.attrs.get('InstrumentType', [b'',])[0].decode('latin-1'))
+
+    @property
+    def sub_type(self):
+        return str(self.hf.attrs.get('InstSubType', [b'',])[0].decode('latin-1'))
+
+    @property
+    def serial(self):
+        return str(self.hf.attrs.get('InstSerial#', [b'',])[0].decode('latin-1'))
+
+    @serial.setter
+    def serial(self, number):
+        self.hf.attrs['InstSerial#'] = np.array([str(number).encode()], dtype='S')
+        self.hf.flush()
+
     def __init__(self, path):
         self.path = path
         self.hf = h5py.File(path, 'r', swmr=True)
+
+    def __repr__(self):
+        return "<%s %s [no. %s]>" % (self.inst_type, self.sub_type, self.serial)
 
     def __iter__(self):
         # TODO :: optimize: gib eine 'smarte' Series zurueck, die sich die aufgerufenen
@@ -90,6 +150,26 @@ class H5Reader(Iterable):
     
         return pd.concat(frames, axis='columns')
     
+    def act_values(self, cursor):
+        info = self.h5f.get('AddTraces/TofSupply/Info')
+        data = self.h5f.get('AddTraces/TofSupply/Data')
+        if not data.shape[1] == info.shape[1]:
+            log.error("No TOF-Supply data available in %r!" % self)
+            return dict()
+
+        keys = [s.decode('latin-1') for s in info[0, :] if s]
+        units = [s.decode('latin-1') for s in info[1, :] if s]
+        values = data[cursor]
+        if keys[0].endswith('[Set]'):
+            rv = {key[:-5]: (value, unit)
+                  for key, value, unit in zip(keys, values, units)
+                  if key.endswith('[Set]')}
+        else:
+            rv = {key: (value, unit)
+                  for key, value, unit in zip(keys, values, units)}
+
+        return rv
+
     def get_traces(self, kind='raw', force_original=False):
         if force_original:
             return self._read_traces(kind)
