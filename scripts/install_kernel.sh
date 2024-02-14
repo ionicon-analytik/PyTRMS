ec5139c7e24f063dccc841541a053ed9ffb3b7de
Moritz Koenemann <moritz.koenemann@ionicon.com>
2024-02-14 15:47:36 +0100

    Move shell scripts to 'devops'.

--- scripts/install_kernel.sh
+++
@@ -1,7 +0,0 @@
-# install a ipython kernel in the local venv
-#
-# run this once, then use it like this:
-# >> jupyter qtconsole --kernel=pytrms-test
-
-poetry run python -m ipykernel install --user --name pytrms-test --display-name "Python (pytrms)"
-
