9dd19b111d6117aee7c61c91942dff6798b743a9
Moritz Koenemann <moritz.koenemann@ionicon.com>
2025-09-17 16:57:42 +0200

    No more coupling from 'Instrument' to 'Measurement'.
    > das war mir irgendwie zu dumm! Die Instrument Klasse
      soll sich nur um das PTR-Instrument kuemmern, nicht um die Daten.

--- pytrms/instrument.py
+++ pytrms/instrument.py
@@ -1,11 +1,12 @@
+"""
+@file instrument.py
+
+"""
 import os.path
 import time
 from abc import abstractmethod, ABC
 
-from .measurement import (
-        RunningMeasurement,
-        FinishedMeasurement,
-)
+from ._base import _IoniClientBase
 
 __all__ = ['Instrument']
 
@@ -14,13 +15,35 @@ class Instrument(ABC):
     '''
     Class for controlling the PTR instrument remotely.
 
-    This class reflects the states of the actual instrument, which can be currently idle
-    or running a measurement. An idle instrument can start a measurement. A running
-    instrument can be stopped. But trying to start a measurement twice will raise an
-    exception (RuntimeError).
+    This class reflects the states of the actual instrument, which can be currently
+    either idle or running. A idle instrument can start a measurement. A running
+    instrument can be stopped.
 
-    Note, that for every client PTR instrument there is only one instance of this class.
-    This is to prevent different instances to be in other states than the instrument.
+    The `Instrument` class wraps a `backend`. For testing purposes, we use a mock:
+    >>> from pytrms.clients import dummy
+    >>> backend = dummy.IoniDummy()
+
+    Note, that for every client PTR instrument there is only one instance of this class
+     (this is to prevent different instances to be in other states than the instrument).
+
+    >>> ptr = Instrument(backend)
+    >>> ptr
+    <_IdleInstrument [<IoniDummy @ 127.0.0.1[:1234]>]>
+
+    >>> id(ptr) == id(Instrument(dummy.IoniDummy()))  # singleton ID is always the same
+    True
+
+    Trying to start an instrument twice will raise a RuntimeError!
+
+    >>> ptr.start_measurement(filename='foo %y %M')
+    >>> ptr.is_running
+    True
+
+    >>> ptr.start_measurement()
+    Traceback (most recent call last):
+        ...
+    RuntimeError: can't start <_RunningInstrument>
+
     '''
     __instance = None
 
@@ -28,7 +51,7 @@ class Instrument(ABC):
         # Note: we get ourselves a nifty little state-machine :)
         self.__class__ = newstate
 
-    def __new__(cls, backend):
+    def __new__(cls, *args, **kwargs):
         # Note (reminder): If __new__() does not return an instance of cls,
         #  then the new instance’s __init__() method will *not* be invoked!
         #
@@ -37,6 +60,9 @@ class Instrument(ABC):
         #  to one of its subclass implementations.
         if cls._Instrument__instance is not None:
             return cls._Instrument__instance
+
+        backend = args[0]  # fetch it from first argument (passed to __init__)
+        assert isinstance(backend, (_IoniClientBase)), f"backend must implement {type(_IoniClientBase)}"
 
         if backend.is_running:
             inst = object.__new__(_RunningInstrument)
@@ -47,15 +73,22 @@ class Instrument(ABC):
 
         return inst
 
-    def __init__(self, backend):
-        # Note: this will be called *once* per Python process!
-        self.backend = backend
+    @property
+    def is_running(self):
+        return type(self) == _RunningInstrument
 
     @property
     def is_local(self):
         """Returns True if files are written to the local machine."""
         host = str(self.backend.host)
         return 'localhost' in host or '127.0.0.1' in host
+
+    def __init__(self, backend):
+        # Note: this will be called *once* per Python process!
+        self.backend = backend
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__} [{self.backend}]>'
 
     def get(self, varname):
         """Get the current value of a setting."""
@@ -76,15 +109,29 @@ class Instrument(ABC):
         """Set a variable to a new value."""
         return self.backend.set(varname, value, unit='-')
 
-    _current_sourcefile = ''
-
     def start_measurement(self, filename=''):
+        """Start a new measurement.
+
+        'filename' is the filename of the datafile to write to. If left blank, start
+        a "quick measurement", for which IoniTOF writes to its default folder.
+
+        If pointing to a file and the file exist on the (local) server, this raises
+        an exception! To create unique filenames, use placeholders for year (%Y),
+        month (%m), and so on, for example `filename=D:/Sauerteig_%Y-%m-%d_%H-%M-%S.h5`.
+        The `filename` is passed through `strftime` with the current date and time.
+
+        see also:
+        """
         # this method must be implemented by each state
-        raise RuntimeError("can't start %s" % self.__class__)
+        raise RuntimeError("can't start <%s>" % type(self).__name__)
+
+    # (see also: this docstring)
+    start_measurement.__doc__ += time.strftime.__doc__
 
     def stop_measurement(self):
+        """Stop a running measurement."""
         # this method must be implemented by each state
-        raise RuntimeError("can't stop %s" % self.__class__)
+        raise RuntimeError("can't stop <%s>" % type(self).__name__)
 
 
 class _IdleInstrument(Instrument):
@@ -110,18 +157,12 @@ class _IdleInstrument(Instrument):
                 raise RuntimeError(f'filename exists and cannot be overwritten')
 
         self.backend.start_measurement(filename)
-        self._current_sourcefile = filename
-        self._new_state(RunningInstrument)
-
-        return RunningMeasurement(self)
+        self._new_state(_RunningInstrument)
 
 
 class _RunningInstrument(Instrument):
 
     def stop_measurement(self):
         self.backend.stop_measurement()
-        self._new_state(IdleInstrument)
-
-        # TODO :: this catches only one sourcefile.. it'll do for simple cases:
-        return FinishedMeasurement(self._current_sourcefile)
+        self._new_state(_IdleInstrument)
 
